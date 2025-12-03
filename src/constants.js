d9f77c37a290d013583b35498eaaab48e297f751
itsOmkara <9403139921om@gmail.com>
2025-12-03 19:44:13 +0530

    Initial commit: my portfolio

---
+++ src/constants.js
@@ -0,0 +1,237 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import Vaultmate from './assets/Screenshot_2025-08-11_210527.png'
+import Banking from './assets/login.png'
+import CarRental from './assets/HomePage.png.png'
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+import IMSLOGO from './assets/IMSLOGO.png';
+import WITLOGO from './assets/WITLOGO.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      // { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      // { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      // { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      // { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      // { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      // { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Fullstack Developer",
+      company: "Webverse Digital",
+      date: "April 2024 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Fullstack Engineer",
+      company: "Agumentik Group of Companies",
+      date: "July 2023 - March 2024",
+      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Frontend Intern",
+      company: "Newton School",
+      date: "September 2021 - August 2022",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: WITLOGO,
+      school: "Walchand Institute of Technology, Solapur",
+      date: "2020 - 2024",
+      grade: "9.34 CGPA",
+      desc: "I completed my Bachelor's degree in Electronics Engineering with Honours in Artificial Intelligence and Machine Learning from WIT Solapur. I gained hands-on experience in software development, machine learning, and core electronics. I also participated in internships, technical activities, and sports representing the college.",
+      degree: "B.Tech in Electronics Engineering (Honours in AIML)",
+    },
+    {
+      id: 1,
+      img: IMSLOGO,
+      school: "Indian Model Junior College, Solapur",
+      date: "2020",
+      grade: "72%",
+      desc: "I completed my Class 12th with Physics, Chemistry, and Mathematics. Engaged in practical-based learning, group projects, and technical events that strengthened interest in engineering and technology.",
+      degree: "HSC (XII) - PCM + Computer Science",
+    },
+    {
+      id: 2,
+      img: IMSLOGO,
+      school: "Indian Model School, Akkalkot",
+      date: "2018",
+      grade: "87%",
+      desc: "I completed my Class 10th with excellent academic performance.Secured First Class with Distinction, demonstrating strong academic foundation.Actively participated in school-level science exhibitions and sports activities, building teamwork and leadership qualities.",
+      degree: "SSC (X)",
+    },
+  ];
+  
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Online Banking System",
+      description:
+        "The Online Banking System is a web-based application that allows users to manage their bank accounts, transfer funds, and view transaction history. It is built using Java Servlets, MySQL, and HTML/CSS/JavaScript.",
+      image: Banking,
+      tags: ["HTML", "CSS", "JavaScript", "JDBC", "Java", "Servlet"],
+      github: "https://github.com/ItsOmkara/Online-Banking-System",
+      webapp: "",
+    },
+    {
+      id: 1,
+      title: "Vaultmate(Money Manager)",
+      description:
+        "Built a full-stack personal finance management web application using Spring Boot, MySQL, React, and REST API.Implemented secure user registration with email verification and JWT-based authentication, along with a responsive React dashboard that enables users to easily manage their finances through CRUD operations, transaction history, and expense categorization. Additionally, optimized RESTful APIs ensure smooth client-server communication and seamless tracking and filtering of income and expenses.",
+      image: Vaultmate,
+      tags: ["React JS", "Springboot", "Java" ,"MySQL", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/ItsOmkara/VaultMate-Backend",
+      webapp: "https://vaultmate.netlify.app",
+    },
+    {
+      id: 2,
+      title: "CarRental Webapp",
+      description:
+      "RideEase is a sleek and user-friendly car rental platform designed to provide a seamless experience for users looking to rent vehicles. The website offers a diverse fleet of well-maintained cars, ensuring that customers can find the perfect vehicle for their needs.",
+      image: CarRental,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/codingmastr/Movie-Recommendation-App",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+  ];  
